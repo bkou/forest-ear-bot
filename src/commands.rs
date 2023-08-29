a3008f26ec5f6e0039a94d63f1e36cd9ac0764ab
bkou <1508416+bkou@users.noreply.github.com>
2023-08-28 22:22:41 -0700

    Initial commit - restart, screenshot, kill_process, list_process

---
+++ src/commands.rs
@@ -0,0 +1,4 @@
+pub mod restart;
+pub mod screenshot;
+pub mod list_process;
+pub mod kill_process;
---
+++ src/commands/screenshot.rs
@@ -0,0 +1,34 @@
+use serenity::framework::standard::macros::command;
+use serenity::framework::standard::{Args, CommandResult};
+use serenity::model::prelude::*;
+use serenity::prelude::*;
+use screenshots::Screen;
+use std::env;
+
+#[command]
+pub async fn screenshot(ctx: &Context, msg: &Message, _args: Args) -> CommandResult {
+    let screens = Screen::all().unwrap();
+    let image = screens[0].capture().unwrap();
+    let temp_file = env::temp_dir().join("forest_bot_screenshot.png");
+    image.save(&temp_file).unwrap();
+
+    let msg = msg
+        .channel_id
+        .send_message(&ctx.http, |m| {
+            m.embed(|e| {
+                e.image("attachment://forest_bot_screnshot.png")
+                    .footer(|f| f.text(format!("{}", temp_file.display())))
+                    // Add a timestamp for the current time
+                    // This also accepts a r/fc3339 Timestamp
+                    .timestamp(Timestamp::now())
+                })
+                .add_file(&temp_file)
+        })
+        .await;
+
+    if let Err(why) = msg {
+        println!("Error sending message: {:?}", why);
+    }
+
+    Ok(())
+}
