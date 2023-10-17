bdbc20409a4ac29ab64d379b972e750ebe4039c5
bkou <1508416+bkou@users.noreply.github.com>
2023-10-16 22:58:33 -0700

    modify backup to return all existing saves rather than just latest

--- src/commands/backup.rs
+++ src/commands/backup.rs
@@ -47,25 +47,28 @@ pub async fn backup(ctx: &Context, msg: &Message) -> CommandResult {
                 .unwrap()
                 .metadata()
                 .unwrap()
+
                 .created()
                 .unwrap()
+
                 .cmp(&b.as_ref().unwrap().metadata().unwrap().created().unwrap()))
         );
-        let last_backup = &paths.last().unwrap();
-        // No fucking clue why this can't go in the block, has to be defined or "does not live long
-        // enough".
-        let path = last_backup.as_ref().clone().unwrap().path();
-        let path_str = String::from(path.clone().to_str().unwrap());
-        if let Err(why) = msg
-            .channel_id
-            .send_message(
-                &ctx.http, |m| {
-                    m.content(format!("{:?}", path_str))
-                        .add_file(path_str.as_str())
-            })
-            .await
-        {
-            println!("Error sending message: {:?}", why);
+        for backup in &paths {
+            // No fucking clue why this can't go in the block, has to be defined or "does not live long
+            // enough".
+            let path = backup.as_ref().clone().unwrap().path();
+            let path_str = String::from(path.clone().to_str().unwrap());
+            if let Err(why) = msg
+                .channel_id
+                .send_message(
+                    &ctx.http, |m| {
+                        m.content(format!("{:?}", path_str))
+                            .add_file(path_str.as_str())
+                })
+                .await
+            {
+                println!("Error sending message: {:?}", why);
+            }
         }
     }
     Ok(())
