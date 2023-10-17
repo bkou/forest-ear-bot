cb288e9ad7e39cba6b76425680da659726099de4
bkou <1508416+bkou@users.noreply.github.com>
2023-10-16 22:25:05 -0700

    bug fix - resatrt actually shutdown instead

--- src/commands/restart.rs
+++ src/commands/restart.rs
@@ -2,11 +2,11 @@ use serenity::framework::standard::macros::command;
 use serenity::framework::standard::{Args, CommandResult};
 use serenity::model::prelude::*;
 use serenity::prelude::*;
-use system_shutdown::shutdown;
+use system_shutdown::reboot;
 
 #[command]
 pub async fn restart(_ctx: &Context, _msg: &Message, _args: Args) -> CommandResult {
-    match shutdown() {
+    match reboot() {
         Ok(_) => println!("Shutting down, bye!"),
         Err(error) => eprintln!("Failed to shut down: {}", error),
     }
